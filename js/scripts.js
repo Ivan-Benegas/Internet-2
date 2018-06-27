fa515f7edcae345efcba4c098c794a5ad877d2fd
Ivan <ivanbenegas1996@gmail.com>
2018-06-27 17:14:55 -0300

    ultimo

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,27 @@
 var Table = {
-data:[],
+    data:[],
 
-setData:function(Data){
-    Table.data = Data;
-},
-listAll:function(){
-    for(var i = 0; i < Table.data.length; i++){
-        $("#table-body").append(Table.assamble(Table.data[i]));
-    }
-},
-assamble:function(Data){
-    return '<div class="row">' +
+
+    setData:function(Data){
+        Table.data = Data;
+    },
+
+
+    limpiartabla:function(){
+        $("#table-body").html("");
+    },
+
+
+    listAll:function(){
+        Table.limpiartabla();
+        for(var i = 0; i < Table.data.length; i++){
+            $("#table-body").append(Table.assamble(Table.data[i]));
+        }
+    },
+
+
+    assamble:function(Data){
+        return '<div class="row">' +
                 '<span class="col-2">' + '</span>' +
                 '<span class="col-2">' + Data.brand + '</span>' +
                 '<span class="col-2">' + Data.location + '</span>' +
@@ -18,32 +29,88 @@ assamble:function(Data){
                 '<span class="col-2">' + Data.price + '</span>' +
                 '<span class="col-2">' + '</span>' +
                 '<span class="description col-12">' + "" + '</span>' +
-                '</div>';
- },
+            '</div>';
+    },
+
+
+    listByBrand:function(){
+        Table.limpiartabla();
+        var input = document.getElementById("BrandInput").value;
+        if(input == ""){
+            Table.listAll();
+        }
+        for(var i = 0; i < Table.data.length; i++){
+            if(Table.data[i].brand == input){
+                $("#table-body").append(Table.assamble(Table.data[i]));
+            }
+        }
+    },
+
+
+    listByLocation:function(){
+        Table.limpiartabla();
+        var input = document.getElementById("LocationInput").value;
+        if(input == ""){
+            Table.listAll();
+        }
+        for(var i = 0; i < Table.data.length; i++){
+            if(Table.data[i].location == input){
+                $("#table-body").append(Table.assamble(Table.data[i]));
+            }
+        }
+    },
+
+
+    listByLaunch:function(){
+        Table.limpiartabla();
+        var input = document.getElementById("LaunchInput").value;
+        if(input == ""){
+            Table.listAll();
+        }
+        for(var i = 0; i < Table.data.length; i++){
+            if(Table.data[i].lunch == input){
+                $("#table-body").append(Table.assamble(Table.data[i]));
+            }
+        }
+    },
+
+
+    listByPrice:function(){
+        Table.limpiartabla();
+        var input = document.getElementById("PriceInput").value;
+        if(input == ""){
+            Table.listAll();
+        }
+        for(var i = 0; i < Table.data.length; i++){
+            if(Table.data[i].price == input){
+                $("#table-body").append(Table.assamble(Table.data[i]));
+            }
+        }
+    },
 }
 
-function tabla () {
+
+$("#action1").on("click", function(){
+    Table.listByBrand();
+})
+
+
+$("#action2").on("click", function(){
+    Table.listByLocation();
+})
+
+
+$("#action3").on("click", function(){
+    Table.listByLaunch();
+})
+
+
+$("#action4").on("click", function(){
+    Table.listByPrice();
+})
+
+
+function tabla() {
     Table.setData(elementsList);
     Table.listAll();
-}
-
-var ListElements={
-ListByPrice:function(price){
-    for(var i = 0 ; i < ListElements.items.length ; i++){
-        if(ListElements.items[i].price < price){
-            $("#table-body").append(ListElements.assamble(ListElements.items[i]));
-        }
-    }
-}
-
-$("#action1").on("click",function(){
-    var Price = $("#Price") Valor(),
-    ListElements.ListByPrice(price);
-})
-}
-
-ListElements.setData(elementsList)
-
-cleartable:function(){
-    $("#table-body") HTML("")
-}
+};
